78dabedefe358d4b648f78f7fcbbd5822d046422
Sandor Zoletnik <zoletnik.sandor@wigner.mta.hu>
2019-07-04 00:45:33 +0200

    Correction in slice_data: slicing points outside of coordinate range removed as it produced problems in interpolation.

--- flap/data_object.py
+++ flap/data_object.py
@@ -1652,10 +1652,16 @@ class DataObject:
                             ind_coord = (slicing_arr - slicing_coord.start) / slicing_coord.step
                             ind = np.nonzero(ind_coord < 0)[0]
                             if (ind.size > 0):
-                                ind_coord[ind] = 0
+                                ind = np.nonzero(ind_coord >= 0)[0]
+                                if (len(ind) == 0):
+                                    raise ValueError("SLicing is out of coordinnate range.")
+                                ind_coord = ind_coord[ind]
                             ind = np.nonzero(ind_coord >= data_shape[slicing_coord.dimension_list[0]])[0]
                             if (ind.size > 0):
-                                ind_coord[ind] = data_shape[slicing_coord.dimension_list[0]] - 1
+                                ind = np.nonzero(ind_coord < data_shape[slicing_coord.dimension_list[0]])[0]
+                                if (len(ind) == 0):
+                                    raise ValueError("Slicing is out of coordinnate range.")
+                                ind_coord = ind_coord[ind]
                         else:
                             # Checking for monotonicity of coordinate
                             diff = (coord_data[1:] - coord_data[:-1])
