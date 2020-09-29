9cfd4d621184ef2ebb23dafb5d927c6efcba9ef4
Gabor Cseh <cseh.gabor@wigner.mta.hu>
2020-09-29 13:31:42 +0200

    Documentation is regenerated based on the latest master commits.

--- docs/code-docs/_build/epub/_static/documentation_options.js
+++ docs/code-docs/_build/epub/_static/documentation_options.js
@@ -3,7 +3,9 @@ var DOCUMENTATION_OPTIONS = {
     VERSION: '',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
+    BUILDER: 'epub',
     FILE_SUFFIX: '.xhtml',
+    LINK_SUFFIX: '.xhtml',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false
