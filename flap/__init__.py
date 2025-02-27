a94569cd81e7d6c225d3bdd5ef3fdfd02fa83d35
sandorzoletnik <zoletnik.sandor@ek-cer.hu>
2025-02-27 20:51:43 +0100

    From tools.py added only functions of common interest to __init__.py

--- flap/__init__.py
+++ flap/__init__.py
@@ -1,6 +1,6 @@
 global VERBOSE
 VERBOSE = True
-from .tools import *
+from .tools import select_signals,SignalList,interpret_signals,time_unit_translation,spatial_unit_translation,unit_conversion
 from .coordinate import *
 from .data_object import *
 from .plot import *
